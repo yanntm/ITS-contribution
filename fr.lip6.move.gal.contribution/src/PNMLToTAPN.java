5df47101ad4c5517fe96231345655ac27a8fddf7
JustinRudat <rudat.justin@gmail.com>
2018-03-07 01:37:10 +0100

    adding a select file option and ...
    - adding creation of temporary file with unique name
    - launching and showing query results
    - adding of comments.

--- fr.lip6.move.gal.contribution/src/PNMLToTAPN.java
+++ fr.lip6.move.gal.contribution/src/PNMLToTAPN.java
@@ -1,9 +1,11 @@
- 
+
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,121 +20,141 @@ import fr.lip6.move.pnml.ptnet.PnObject;
 import fr.lip6.move.pnml.ptnet.Transition;
 
 public class PNMLToTAPN {
-    
-	public PetriNet net;
-	public ArrayList<Arc> input_arcs;
-	public ArrayList<Arc> output_arcs;
-	public PrintWriter pw;
-	public List<String> orders;
-	
-	
-	public PNMLToTAPN(String path,String newpath, List<String> orders){
-	    this.orders=orders;
-		File file = new File(path);
-		File file_tmp = new File(newpath);
-		try {
-            this.pw = new PrintWriter(file_tmp);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+
+    public PetriNet net;
+    public String net_name;
+    public ArrayList<Arc> input_arcs;
+    public ArrayList<Arc> output_arcs;
+    public PrintWriter pw;
+    public FileWriter fw;
+    public List<String> orders;
+
+
+    public PNMLToTAPN(String path,String newpath, List<String> orders){
+        
+        this.orders=orders;                                 // a list of orders calculated with a specific heuristic
+        File file = new File(path);                         // linking from the pnml file path
+        File file_tmp = new File(newpath);                  // linking the temporary file for the verifypn64 engine
+        this.input_arcs = new ArrayList<>();                // initializing differents arraylist
+        this.output_arcs = new ArrayList<>();               //
+        try {
+
+            this.pw = new PrintWriter(file_tmp);            // creating a prinwriter to write on the temporary file
+        } catch (IOException e) {
+            System.err.println("I/O Error while initializing a PNMLToTAPN instance.");
         }
-		PTNetReader ptreader = new PTNetReader();
-		PetriNet net = null;
-		try {
+        PTNetReader ptreader = new PTNetReader();           // creation of a Parser/Writer for PetriNet
+        PetriNet net = null;
+        try {
+                                                            // retrieving of the net from the pnml file
             this.net = ptreader.loadFromXML(new BufferedInputStream(new FileInputStream(file.getPath())));
+            
         } catch (NotAPTException | FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-		
-	}
-	
-	
-	public void toTAPN(){
-		if(null==net){
-			System.out.println("Pas de net en présence.\n");
-			return;
-		}
-		pw.println("<pnml>\n");
-		pw.println("<net id=\"" + net.getName() + "\" type=\"P/T net\">\n"); 
-				
-		for (Page p : net.getPages()) {
-			handlePage(p,pw);
-		}
-		for (Arc a : input_arcs){
-			pw.print("<inputArc ");
-			pw.print("inscription=\""+a.getInscription().toString()+"\" ");
-			pw.print("source=\"" + a.getSource().toString() + "\" ");
-			pw.println("target=\"" + a.getTarget().toString() + "\" />");
-		}
-		for (Arc a : output_arcs){
-			pw.print("<outputArc ");
-			pw.print("inscription=\""+a.getInscription().toString()+"\" ");
-			pw.print("source=\"" + a.getSource().toString() + "\" ");
-			pw.println("target=\"" + a.getTarget().toString() + "\" />");
-		}
-		pw.println("</net>\n");
-		pw.println("</pnml>");
-	}
-	
-	public void handlePage(Page page, PrintWriter pw){
-	    boolean places=false;
-	    ArrayList<Place> places_array = new ArrayList<>();
-	    ArrayList<Transition> transitions = new ArrayList<>();
-		for (PnObject n : page.getObjects()) {
-			if (n instanceof Place) {
-				Place p = (Place) n;
-				places_array.add(p);
-				pw.print("<place ");
-	            pw.print("id=\"" + p.getId() + "\" ");
-	            pw.print("name=\"" + p.getId() + "\" ");
-	            pw.print("invariant=\"&lt; inf\" ");
-	            pw.print("initialMarking=\""+p.getInitialMarking().getText()+"\"");
-	            pw.println("/>");
-			}
-			if (n instanceof Transition) {
-				Transition t = (Transition) n;
-				transitions.add(t);
-				
-				for(Arc a : ((Transition) n).getInArcs()){
-					input_arcs.add(a);
-				}
-				for(Arc a : ((Transition) n).getOutArcs()){
-					output_arcs.add(a);
-				}
-				
-			}
-		}
-//		if(!places.isEmpty()) {
-//		    String orders="";
-//		    handleOrder(places,orders);
-//		}
-		if(!transitions.isEmpty()) {
-		    for(Transition t: transitions) {
-		        pw.print("<transition ");
-                pw.print("id=\"" + t.getId() + "\" ");
-                pw.print("name=\"" + t.getId() + "\" ");
-                pw.println("urgent=\"false\" />");
-		    }
-		}
-	}
-	
-	public void handleOrder(ArrayList<Place> places) {
-	    while(!orders.isEmpty()) {
-    	    for(Place place : places) {
-    	        if(orders.get(0).equals(place.getName())) {
-        	        pw.print("<place ");
-        	        pw.print("id=\"" + place.getId() + "\" ");
-        	        pw.print("name=\"" + place.getId() + "\" ");
-        	        pw.print("invariant=\"&lt; inf\" ");
-        	        pw.print("initialMarking=\""+place.getInitialMarking().getText()+"\"");
-        	        pw.println("/>");
-        	        orders.remove(0);
-    	        }
-    	    }
-	    }
-	}
-	
-	
-	
+        
+    }
+
+    // Converting a pnml file into a temporary xml file structured as a pnml for the TAPAAL engine
+    public void toTAPN(){
+        if(null==net){
+            System.err.println("Net is null.\n");
+            return;
+        }
+        this.net_name = net.getName().getText().toString().replace("-","_dash_") ;              // normalizing the net name and id with TAPAAL's norm
+        pw.println("<pnml>");                                                                   // and defining name of the net and the file's header 
+        pw.println("<net id=\"" + net_name + "\" type=\"P/T net\">"); 
+
+        for (Page p : net.getPages()) {                                                         // Loop handling the net's pages
+            handlePage(p,pw);
+        }
+        
+        // the input and output arc are stored now and can be dealt with to respect TAPAAL's format
+        for (Arc a : input_arcs){                                                               // Dealing with input arcs
+            pw.print("<inputArc ");
+            pw.print("source=\"" + net_name +"_"+a.getSource().getId().toString() + "\" ");
+            pw.print("target=\"" + net_name +"_"+a.getTarget().getId().toString() + "\" >");
+            pw.println("<inscription><value>1</value></inscription></inputArc>");
+        }
+        for (Arc a : output_arcs){                                                              // Dealing with output arcs
+            pw.print("<outputArc ");
+            pw.print("source=\"" + net_name +"_"+a.getSource().getId().toString() + "\" ");
+            pw.print("target=\"" + net_name +"_"+a.getTarget().getId().toString() + "\" >");
+            pw.println("<inscription><value>1</value></inscription></outputArc>");
+        }
+        pw.println("</net>");                                                                   // writing the eng of the file
+        pw.println("</pnml>");  
+        pw.flush();                                                                             // Always remember to flush...
+    }
+    
+    
+    // PageHandler based on Lip6 GAL's contribution adapted to use orders and for TAPAAL engine
+    public void handlePage(Page page, PrintWriter pw){
+        boolean places=false;
+        ArrayList<Place> places_array = new ArrayList<>();
+        ArrayList<Transition> transitions = new ArrayList<>();
+        for (PnObject n : page.getObjects()) {
+            if (n instanceof Place) {
+                Place p = (Place) n;
+                places_array.add(p);
+
+            }
+            if (n instanceof Transition) {
+                Transition t = (Transition) n;
+                transitions.add(t);
+
+                for(Arc a : ((Transition) n).getInArcs()){
+                    input_arcs.add(a);
+                }
+                for(Arc b : ((Transition) n).getOutArcs()){
+                    output_arcs.add(b);
+                }
+
+            }
+        }
+
+        handleOrder(places_array);
+
+        if(!transitions.isEmpty()) {
+            for(Transition t: transitions) {
+                pw.print("<transition ");
+                pw.print("id=\"" + net_name+"_"+t.getId() + "\" ");
+                pw.print("name=\"" + net_name+"_"+t.getId() + "\" ");
+                pw.print("urgent=\"false\"/>");
+            }
+        }
+    }
+    
+    
+    //TODO not tested yet, expect for orders -> null
+    // Handler for rewriting the temporary xml file 
+    public void handleOrder(ArrayList<Place> places) {
+        if(null==orders||orders.isEmpty()) {                                                        // If there is not specific order defined
+            for(Place p : places) {                                                                 // handle the order the original 
+                pw.print("<place ");                                                                // as the original file was made
+                pw.print("id=\"" + net_name+"_"+p.getId() + "\" ");
+                pw.print("name=\"" + net_name+"_"+p.getId() + "\" ");
+                pw.print("invariant=\"&lt; inf\" ");
+                pw.print("initialMarking=\""+p.getInitialMarking().getText()+"\"");
+                pw.println(" />");
+            }
+        }else {                                                                                    // Else ...!
+            while(!orders.isEmpty()) {                                                             // While we didn't check through all the orders 
+                for(Place place : places) {                                                        // in the list then :
+                    if(orders.get(0).equals(place.getName())) {                                    // We check in the net's places tab which places 
+                        pw.print("<place ");                                                       // come's next and write it in the file
+                        pw.print("id=\"" +net_name+"_"+ place.getId() + "\" ");
+                        pw.print("name=\"" + net_name+"_"+place.getId() + "\" ");
+                        pw.print("invariant=\"&lt; inf\" ");
+                        pw.print("initialMarking=\""+place.getInitialMarking().getText()+"\"");
+                        pw.println(" />");
+                        orders.remove(0);                                                          // Removing the order of the place we dealt with
+                    }
+                }
+            }
+        }
+    }
+
+
+
 }
